898f67e2981167eee40fa297d8c94450cfdf434f
azwarbahar07 <azwarbahar07@gmail.com>
2021-06-16 19:09:41 +0800

    make graph berat badan dan tinggi badan

--- app/src/main/java/com/skripsi/apmodasi/ui/activity/DetailBayiActivity.java
+++ app/src/main/java/com/skripsi/apmodasi/ui/activity/DetailBayiActivity.java
@@ -1,13 +1,41 @@
 package com.skripsi.apmodasi.ui.activity;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
-
 import com.skripsi.apmodasi.R;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class DetailBayiActivity extends AppCompatActivity {
+
+    private RelativeLayout rl_qr_code;
+    // sliding pannel
+    private SlidingUpPanelLayout sliding_layout;
+
+    private LineChartView chart_tb;
+    private LineChartView chart_bb;
+
+    String[] axisData = {"Jan", "Feb", "Mar", "Apr", "Mey", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"};
+    int[] yAxisData = {50, 30, 60, 50, 70, 75, 80, 78, 85, 90, 89, 91};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +46,97 @@ public class DetailBayiActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // sliding pannel
+        sliding_layout = findViewById(R.id.sliding_layout);
+
+
+        chart_tb = (LineChartView) findViewById(R.id.chart_tb);
+        chart_bb = (LineChartView) findViewById(R.id.chart_bb);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        chart_tb.setLineChartData(data);
+        Viewport viewport = new Viewport(chart_tb.getMaximumViewport());
+        viewport.top = 110;
+        chart_tb.setMaximumViewport(viewport);
+        chart_tb.setCurrentViewport(viewport);
+        chart_bb.setOnValueTouchListener(new LineChartOnValueSelectListener() {
+            @Override
+            public void onValueSelected(int i, int i1, PointValue pointValue) {
+                Toast.makeText(DetailBayiActivity.this, "Nilai : " + pointValue.getY(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onValueDeselected() {
+
+            }
+        });
+
+        chart_bb.setLineChartData(data);
+        Viewport viewport_bb = new Viewport(chart_bb.getMaximumViewport());
+        viewport_bb.top = 110;
+        chart_bb.setMaximumViewport(viewport_bb);
+        chart_bb.setCurrentViewport(viewport_bb);
+
+
+//        GraphView graphView = findViewById(R.id.graph_bb);
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+//                new DataPoint(0, 1),
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 3),
+//                new DataPoint(3, 2),
+//                new DataPoint(4, 6)
+//        });
+//        graphView.addSeries(series);
+
+
+        rl_qr_code = findViewById(R.id.rl_qr_code);
+        rl_qr_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPanel();
+            }
+        });
+
+    }
+
+    private void showPanel() {
+        if (sliding_layout != null &&
+                (sliding_layout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED || sliding_layout.getPanelState() == SlidingUpPanelLayout.PanelState.ANCHORED)) {
+            sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        } else {
+            sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.ANCHORED);
+        }
 
     }
 
