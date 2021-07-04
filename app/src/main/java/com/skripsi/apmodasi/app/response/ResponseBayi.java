c6f5dd3f57fe86a9c4634bb6b4998a57b83e2d0c
azwarbahar07 <azwarbahar07@gmail.com>
2021-07-04 23:05:02 +0800

    sas

---
+++ app/src/main/java/com/skripsi/apmodasi/app/response/ResponseBayi.java
@@ -0,0 +1,36 @@
+package com.skripsi.apmodasi.app.response;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+import com.skripsi.apmodasi.data.model.Bayi;
+
+public class ResponseBayi{
+
+	@SerializedName("kode")
+	private String kode;
+
+	@SerializedName("pesan")
+	private String pesan;
+
+	@SerializedName("bayi_data")
+	private List<Bayi> bayiData;
+
+	@SerializedName("result_bayi")
+	private Bayi result_bayi;
+
+	public Bayi getResult_bayi() {
+		return result_bayi;
+	}
+
+	public String getKode(){
+		return kode;
+	}
+
+	public List<Bayi> getBayiData(){
+		return bayiData;
+	}
+
+	public String getPesan() {
+		return pesan;
+	}
+}
---
+++ app/src/main/java/com/skripsi/apmodasi/data/model/Bayi.java
@@ -0,0 +1,83 @@
+package com.skripsi.apmodasi.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Bayi {
+
+	@SerializedName("nomor_bayi")
+	private String nomorBayi;
+
+	@SerializedName("gambar_qr_bayi")
+	private String gambarQrBayi;
+
+	@SerializedName("bunda_id")
+	private String bundaId;
+
+	@SerializedName("tanggal_lahir_bayi")
+	private String tanggalLahirBayi;
+
+	@SerializedName("jenis_kelamin_bayi")
+	private String jenisKelaminBayi;
+
+	@SerializedName("status_bayi")
+	private String statusBayi;
+
+	@SerializedName("foto_bayi")
+	private String fotoBayi;
+
+	@SerializedName("id_bayi")
+	private String idBayi;
+
+	@SerializedName("nama_bayi")
+	private String namaBayi;
+
+	@SerializedName("update_at")
+	private String updateAt;
+
+	@SerializedName("created_at")
+	private String createdAt;
+
+	public String getNomorBayi(){
+		return nomorBayi;
+	}
+
+	public String getGambarQrBayi(){
+		return gambarQrBayi;
+	}
+
+	public String getBundaId(){
+		return bundaId;
+	}
+
+	public String getTanggalLahirBayi(){
+		return tanggalLahirBayi;
+	}
+
+	public String getJenisKelaminBayi(){
+		return jenisKelaminBayi;
+	}
+
+	public String getStatusBayi(){
+		return statusBayi;
+	}
+
+	public String getFotoBayi(){
+		return fotoBayi;
+	}
+
+	public String getIdBayi(){
+		return idBayi;
+	}
+
+	public String getNamaBayi(){
+		return namaBayi;
+	}
+
+	public String getUpdateAt(){
+		return updateAt;
+	}
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+}
